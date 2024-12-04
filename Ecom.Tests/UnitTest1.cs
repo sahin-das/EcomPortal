0c87e5a8d5b3064ad73185bf032cbeedccfab7b7
Sahin Das <sahin.das@coupa.com>
2024-12-04 12:43:43 +0530

    update tests

--- Ecom.Tests/UnitTest1.cs
+++ Ecom.Tests/UnitTest1.cs
@@ -1,15 +1,34 @@
+using EcomPortal.Models.Entities;
+
 namespace Ecom.Tests;
 
 public class Tests
 {
+    private User _user;
+    private Product _product;
+    
     [SetUp]
     public void Setup()
     {
+        _user = new User()
+        {
+            Name = "John",
+            Email = "john@gmail.com",
+            Phone = "123456789",
+        };
+        _product = new Product()
+        {
+            Name = "Apple",
+            Price = 1.99M,
+            Category = "Mobile",
+            Description = "Phone to make calls"
+        };
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        Assert.That(_user, Is.Not.Null);
+        Assert.That(_product, Is.Not.Null);
     }
 }
